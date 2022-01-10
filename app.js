bac4e86189b112b8bd66312d9991a3720ca83c7e
DwayneL <dr2@ameritech.net>
2022-01-10 11:13:35 -0600

    added some styling to css file for font size, color, and background color of div. Also created array in js file

---
+++ app.js
@@ -0,0 +1,22 @@
+let contestantList = [
+    'Kristen Kish',
+    'Chrissy Camba',
+    'Danyelle McPherson',
+    'Josh Valentine',
+    'John Tesar',
+    'Tyler Ward',
+    'Jorel Price',
+    'Sheldon Simeon',
+    'Carla Pellegrino',
+    'Kuniko Yugi',
+    'Micha Fields',
+    'Josie Smith-Malave',
+    'Brooke Williamson',
+    'Lizzie Binder',
+    'CJ Jacobson',
+    'Stefan Richter',
+    'Eliza Gavin',
+    'Bart Vandaele',
+    'Jeff Jew'
+];
+
